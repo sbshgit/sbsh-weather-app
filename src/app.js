9996ab26bd49c4cc04d2b7c0259806af85fe4a76
sbshgit <shiraniradsaba@gmail.com>
2023-08-29 12:55:28 +0430

    show weather condition of the city that you search

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+//create show current time in javascript
 function formatDate(date) {
   let hours = date.getHours();
   if (hours < 10) {
@@ -25,3 +26,30 @@ function formatDate(date) {
 let dateElement = document.querySelector("#date");
 let currentTime = new Date();
 dateElement.innerHTML = formatDate(currentTime);
+// show weather condition of the city that you search
+function displayWeatherCondition(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].main;
+}
+function searchCity(city) {
+  let apiKey = "71e94ed71d67ec65d062e922a11d67dd";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input").value;
+  searchCity(city);
+}
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", handleSubmit);
+searchCity("Karaj");
